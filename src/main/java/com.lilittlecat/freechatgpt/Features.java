24e546c11bdeb0c0fb7fa17690a814299606b5b9
LiLittleCat <luoyukongchan@outlook.com>
2023-05-16 20:30:20 +0800

    feat: modify Features

--- src/main/java/com.lilittlecat/freechatgpt/Features.java
+++ src/main/java/com.lilittlecat/freechatgpt/Features.java
@@ -1,4 +1,4 @@
-package com.lilittlecat.app;
+package com.lilittlecat.freechatgpt;
 
 import lombok.Getter;
 
@@ -8,15 +8,16 @@ import lombok.Getter;
  */
 @Getter
 public enum Features {
-    FREE("free", "🆓", 2f),
+    FREE("free", "🎁", 2f),
     FREE_QUOTA("free_quota", "🔓", 1f),
-    GPT4_SUPPORTED("gpt4_supported", "🧠", 0.5f),
-    MORE_THAN_CHAT("more_than_chat", "💬", 0.5f),
+    GPT4_SUPPORTED("gpt4_supported", "💪", 1f),
+    MORE_THAN_CHAT("more_than_chat", "🎶", 0.5f),
     LOGIN_REQUIRED("login_required", "🔒", -1f),
-    VPN_REQUIRED("vpn_required", "🌐", -1f),
+    VPN_REQUIRED("vpn_required", "🌎", -1f),
     API_KEY_REQUIRED("api_key_required", "🔑", -1f),
     FOLLOW_ON_WECHAT_REQUIRED("follow_on_wechat_required", "👀", -1f),
-    COMMUNITY_RECOMMENDATION("community_recommendation", "🌟", 0.5f);;
+    CHARGE_REQUIRED("charge_required", "💰", -2f),
+    COMMUNITY_RECOMMENDATION("community_recommendation", "🌟", 0.5f);
     private final String value;
     private final String label;
     private final Float score;
